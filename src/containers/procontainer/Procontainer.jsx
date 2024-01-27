4ce7819105941d77fcfe5a5b7768fac35bd5b90c
Siddharth <siddu2003oct@gmail.com>
2024-01-27 15:27:32 +0530

    after norefferer

--- src/containers/procontainer/Procontainer.jsx
+++ src/containers/procontainer/Procontainer.jsx
@@ -11,13 +11,13 @@ const Procontainer = ({title, desc}) => {
                 <p>{desc}</p>
             </div>
             <div className='pf__procontainer-buttons'>
-                <a href="https://github.com/Siddhartha-Devan" target='_blank'  className='pf__procontainer-buttons-a'>
+                <a href="https://github.com/Siddhartha-Devan" target='_blank' rel="noreferrer"  className='pf__procontainer-buttons-a'>
                     <button className='pf__procontainer-buttons-git'>
                         Github
                     </button>
                 </a>
 
-                <a href="https://github.com/Siddhartha-Devan" target='_blank'  className='pf__procontainer-buttons-a'>
+                <a href="https://github.com/Siddhartha-Devan" target='_blank' rel="noreferrer"  className='pf__procontainer-buttons-a'>
                     <button className='pf__procontainer-buttons-view'>
                         View
                     </button>
